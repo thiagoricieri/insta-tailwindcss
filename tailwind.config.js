55a43bacfc588a07dda5d3428f6cfe75b79a8298
Thiago Ricieri <thiago.ricieri@gmail.com>
2024-05-19 16:08:58 +0200

    Lesson 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
+  ],
 }
 
